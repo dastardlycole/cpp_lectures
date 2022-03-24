5a6a1f7eeaabe1edce915a78f8efae4468f318e3
Ifemide Cole <ifemidecole@gmail.com>
2022-03-24 17:23:23 +0000

    24.03.2022

--- main.cpp
+++ main.cpp
@@ -1,80 +1,70 @@
-#include <array>           // std::array
-#include <iostream>        // std::cout, std::cin, std::scanf
-#include <cmath>        // std::cout, std::cin, std::scanf
-#include <utility>         // std::pair
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
 
-constexpr auto NUM_FUNCTIONS = 7;
+// The Sky is blue -> [The, Sky, is, blue]
+// TheSkyBlue
 
-auto           ask_user_continue() -> bool
+using Words = std::vector<std::string>;
+
+auto extract_words(Words& words, std::string text, std::string_view separators)
 {
-        std::cout << "Would you like to continue (y/n): ";
-        char opt {};
-        std::cin >> opt;
-        return opt != 'n';
-}
+        auto   start = text.find_first_not_of(separators);
+        size_t end {};
 
-auto ask_user_arithemtic() -> int
-{
-        std::cout << "Press 'a' for addition." << '\n';
-        std::cout << "Press 's' for subtraction." << '\n';
-        std::cout << "Press 'm' for multiplication." << '\n';
-        std::cout << "Press 'd' for division." << '\n';
-        std::cout << "Press 'r' for remainder." << '\n';
-        std::cout << "Press 'p' for power." << '\n';
-
-        char opt {};
-        std::cin >> opt;
-        switch (opt)
+        while (start != std::string::npos)
         {
-                case 'a': return 1;
-                case 's': return 2;
-                case 'm': return 3;
-                case 'd': return 4;
-                case 'r': return 5;
-                case 'p': return 6;
-                default: return 0;
+                end = text.find_first_of(separators, start + 1);
+                if (end == std::string_view::npos) { end = text.length(); }
+                std::string s {text.substr(start, end - start)};
+                words.push_back(s);
+                start = text.find_first_not_of(separators, end + 1);
         }
 }
-
-auto ask_user_numbers() -> std::pair<float, float>
+auto isOdd(int i)
+        {
+               
+                if (i % 2 == 0)
+                        {return false;}
+                else
+                        {return true;}
+              
+        }
+auto main() -> int
 {
-        std::cout << "Enter two numbers (x y): ";
-        float x {}, y {};
-        std::scanf("%f %f", &x, &y);
-        return std::make_pair(x, y);
-}
+        // std::cout << "Enter some text: ";
+        // std::string input {};
+        // std::getline(std::cin, input);
+        // Words words {};
+        // extract_words(words, input, " \t");
 
-// Task 1 -> Implement the power and remainder operations.
-// Task 2 -> Declare the `res` variable.
-// Task 3 -> Create an array containing functions that perform arithmetic. (Hint: See example)
+        // std::for_each(words.begin(), words.end(), [](auto& w) { std::cout << w << '\n'; });
 
-// Keyword: Function Signature -> ReturnType (Parameters...)
-using BinaryFn = float (*)(float, float);
+        std::vector<int>numbers {};
+        std::cout << "Enter a list of integers(0 to terminate): ";
+        std::string input {};
+        std::getline(std::cin,input);
+        
 
-constexpr auto                                nop(float x, float y) -> float { return 0; }
+                
 
-constexpr auto                                add(float x, float y) -> float { return x + y; }
-constexpr auto                                subtract(float x, float y) -> float { return x - y; }
-constexpr auto                                multiply(float x, float y) -> float { return x * y; }
-constexpr auto                                divide(float x, float y) -> float { return x / y; }
-constexpr auto                                rem(float x, float y) -> float { return fmod(x,y); }
-constexpr auto                                power(float x, float y) -> float { return pow(x,y); }
+        for (char i:input)
+        {       
+                int x = i - '0';  
+                if(x==0)
+                {
+                        break;
+                }
+                if(i !=' ')
+                {
+                        numbers.push_back(x);
+                }
+                
+        }
+        std::for_each(numbers.begin(), numbers.end(), [](auto& w) { std::cout << w << '\n'; });
+        int noOdd = count_if(numbers.begin(), numbers.end(),isOdd);
+        std::cout << "Total no of odd numbers is: "<< noOdd<<"\n";
+        
 
-constexpr std::array<BinaryFn, NUM_FUNCTIONS> FN {nop, add, subtract, multiply, divide, rem, power};
-
-auto                                          main() -> int
-{
-        std::cout << "Calculator v0.3" << '\n';
-        float res {};
-
-        char opt {};
-        do {
-                const auto op     = ask_user_arithemtic();
-                const auto [a, b] = ask_user_numbers();
-                const auto fn     = FN[op];
-                if (op < 1 || op > 6)
-                {std::cout << "Unknown operation selected. Please select again." << '\n';}
-                std::cout << "Result: " << FN[op](a, b) << '\n';
-                if (!ask_user_continue()) { break; }
-        } while (true);
 }
