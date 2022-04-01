7d5f46dde687fedb50c1bd6cb06134d4aff9e605
Ifemide Cole <ifemidecole@gmail.com>
2022-04-01 09:12:30 +0000

    coursework

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 
 #include <vector>
 #include <iostream>
+#include <algorithm>
 
 // Products
 enum class Product
@@ -10,6 +11,7 @@ enum class Product
         Shampoo,
         Vegetables,
         Shoes,
+        Fruits,
         Count,
 };
 struct Item
@@ -17,10 +19,25 @@ struct Item
         std::string name;
         Product type;
         float price;
+        int units;
+        std::string best_before;
 
 };
 struct Inventory
 {
         std::vector<Item> my_items;
 
+        auto add_item(Item const& item)
+        {    
+                my_items.push_back(item);
+                return true;}
+                
+        auto remove_item(Item const& item)        
+        {       
+                auto pos = find(my_items.begin(),my_items.end(),item);
+                my_items.erase(my_items.begin()+pos);
+                return true;
+        }
+
+        auto find_item()
 };
